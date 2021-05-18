7080790a2b51c53d0b0775ce2b0445c0c66d9235
arezoosab1 <arezoo.saberi@trumpf.com>
2021-05-18 13:31:02 +0200

    new python

--- demo.py
+++ demo.py
@@ -1,7 +1,5 @@
 import pandas as pd
-import datetime as dt
 import plotly.express as px  # (version 4.7.0)
-#import plotly.graph_objects as go
 
 import dash  # (version 1.12.0) pip install dash
 import dash_core_components as dcc
@@ -10,45 +8,8 @@ from dash.dependencies import Input, Output
 
 app = dash.Dash(__name__)
 
-SISHours= pd.read_excel('C:\EaaS\SISHours.xlsx')
+df_SIS_Seri5or3= pd.read_excel('C:\EaaS\app\data.csv')
 
-SISHours["Date"]=SISHours["Betrachtungszeitpunkt"].dt.strftime('%Y-%m-%d' )
-df_SIS = SISHours.filter(['EquipNr','Date','Maschinenstandort', 'EquipTyp_Gruppiert', 'IB1', 'IB2', 'NC_Mittel','Strahl_Mittel', ], axis=1)
-df_SIS[['year','month', 'day']] = df_SIS.Date.str.split("-",expand=True,)
-
-df_SIS_mean = df_SIS.groupby(["EquipNr", "Maschinenstandort", "EquipTyp_Gruppiert"], as_index=False)["NC_Mittel", "Strahl_Mittel"].mean()
-df_SIS_mean['NC_Mittel'] = df_SIS_mean['NC_Mittel'] * 365
-df_SIS_mean['Strahl_Mittel'] = df_SIS_mean['Strahl_Mittel'] * 365
-df_SIS_mean['Strahl/NC'] =  (df_SIS_mean.Strahl_Mittel / df_SIS_mean.NC_Mittel) *100
-df_SIS_mean = df_SIS_mean[df_SIS_mean['Strahl/NC'] < 100].reset_index()
-
-#only series 5000 and 3000
-regex5 = r'5\d{3}'
-regex3 = r'3\d{3}'
-
-regex = [regex5, regex3]
-df_SIS_Seri5or3 = df_SIS_mean[df_SIS_mean.EquipTyp_Gruppiert.str.contains('|'.join(regex))].reset_index()
-df_SIS_Seri5or3['type'] = ' '
-
-for i in range (0, len (df_SIS_Seri5or3)):
-    if 'fiber' in df_SIS_Seri5or3['EquipTyp_Gruppiert'].loc[i]:
-        df_SIS_Seri5or3['type'].loc[i] = 'fiber'
-    if not 'fiber' in df_SIS_Seri5or3['EquipTyp_Gruppiert'].loc[i]:
-        df_SIS_Seri5or3['type'].loc[i] = 'CO2'
-
-df_SIS_Seri5or3['top_NC_Mittel'] = ' '
-df_SIS_Seri5or3['top_Strahl_Mittel'] = ' '
-df_SIS_Seri5or3['top_Strahl/NC'] = ' '
-
-for i in range (0, len (df_SIS_Seri5or3)):
-    nc = len(df_SIS_Seri5or3[df_SIS_Seri5or3['NC_Mittel'] > df_SIS_Seri5or3['NC_Mittel'].loc[i]])
-    s = len(df_SIS_Seri5or3[df_SIS_Seri5or3['Strahl_Mittel'] > df_SIS_Seri5or3['Strahl_Mittel'].loc[i]])
-    r = len(df_SIS_Seri5or3[(df_SIS_Seri5or3['NC_Mittel'] > df_SIS_Seri5or3['NC_Mittel'].loc[i]) & (df_SIS_Seri5or3['Strahl_Mittel'] > df_SIS_Seri5or3['Strahl_Mittel'].loc[i])] )
-
-    l = len(df_SIS_Seri5or3)
-    df_SIS_Seri5or3['top_NC_Mittel'].loc[i] = 100 - (nc/l)*100
-    df_SIS_Seri5or3['top_Strahl_Mittel'].loc[i] = 100 - (s/l)*100
-    df_SIS_Seri5or3['top_Strahl/NC'].loc[i] = 100 - (r/l)*100
 
 #finding average lines 
 mean_NC = df_SIS_Seri5or3["NC_Mittel"].mean()
@@ -64,8 +25,6 @@ for i in equip_lst:
     options.append(d)
 
 #------------------------------------------------------------------------------
-                
-#colunms = ['Maschinenstandort', 'EquipTyp_Gruppiert']
 
 # App layout
 app.layout = html.Div([
@@ -73,15 +32,15 @@ app.layout = html.Div([
     html.H1("Machine Performance Benchmark", style={'text-align': 'center'}),
     dcc.Dropdown(id="slct_equip",
                  options= options,
-		 value= 'A0221E0260',
+		         value= 'A0221E0260',
                  multi=False,
                  style={'width': "40%"}
                  ),
     dcc.Dropdown(id="slct_clnm",
                  options=[{'label': 'Country', 'value': 'Maschinenstandort'},
-			  {'label': 'Machine DevCode', 'value': 'EquipTyp_Gruppiert'},
-			  {'label': 'Machine Type', 'value': 'type'}],
-		 value= 'Maschinenstandort',
+			              {'label': 'Machine DevCode', 'value': 'EquipTyp_Gruppiert'},
+			              {'label': 'Machine Type', 'value': 'type'}],
+		          value= 'Maschinenstandort',
                  multi=False,
                  style={'width': "40%"}
                  ),
